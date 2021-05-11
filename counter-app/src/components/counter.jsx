a2ee45c34eb4e2e911a50fd075c05b0d5d7ca4bf
UchiTesing <uchitesting57@gmail.com>
2021-05-11 18:40:00 +0200

    Added a line in render method to showcase conditional rendering with AND operator.

--- counter-app/src/components/counter.jsx
+++ counter-app/src/components/counter.jsx
@@ -15,6 +15,7 @@ class Counter extends Component {
     render() {
         return (
             <React.Fragment>
+                {this.state.tags.length === 0 && "This displays only if the tags array is empty."}
                 <span className={this.getBadgeClasses()}>{this.formatCount()}</span><br/>
                 <button className="btn btn-secondary btn-sm m-2">Increment</button>
                 {this.renderTags()}
@@ -23,15 +24,13 @@ class Counter extends Component {
     }
 
     renderTags() {
-        if (this.state.tags.length === 0) return <p className="m-2">There is no tag to display</p>;
+        if (this.state.tags.length === 0) return <p className="m-2">There is no tag to display.</p>;
 
         return (
             <ul className="m-2">
                 {this.state.tags.map(tag => <li key={tag}>{tag}</li>)}
             </ul>
         )
-
-
     }
 
     getBadgeClasses() {
