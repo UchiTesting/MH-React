3f1ca924d8eca4e55426d88e497654e0b3025001
UchiTesing <uchitesting57@gmail.com>
2021-05-12 16:20:16 +0200

    Renamed count to value. Added a header populated from props.id.

--- counter-app/src/components/counter.jsx
+++ counter-app/src/components/counter.jsx
@@ -2,8 +2,7 @@ import React, { Component } from 'react';
 
 class Counter extends Component {
     state  = {
-        count: 0,
-        // tags: ['tag1','tag2','tag3']
+        value: this.props.value,
         tags: []
     }
 
@@ -15,6 +14,7 @@ class Counter extends Component {
     render() {
         return (
             <React.Fragment>
+                <h6>Counter #{this.props.id}</h6>
                 <span className={this.getBadgeClasses()}>{this.formatCount()}</span><br/>
                 <button onClick={this.handleDecrement} className="btn btn-secondary btn-sm m-2">Decrement</button>
                 <button onClick={this.handleIncrement} className="btn btn-secondary btn-sm m-2">Increment</button>
@@ -26,13 +26,13 @@ class Counter extends Component {
     handleDecrement = (product) => {
         console.log(product);
 
-        this.setState({count: this.state.count - 1});
+        this.setState({value: this.state.value - 1});
     }
 
     handleIncrement = (product) => {
         console.log(product);
 
-        this.setState({count: this.state.count + 1});
+        this.setState({value: this.state.value + 1});
     }
 
     renderTags() {
@@ -47,13 +47,13 @@ class Counter extends Component {
 
     getBadgeClasses() {
         let classes = "badge m-2 badge-";
-        classes += this.state.count === 0 ? "warning" : "primary";
+        classes += this.state.value === 0 ? "warning" : "primary";
         return classes;
     }
 
     formatCount() {
-        const {count} = this.state;
-        return count === 0 ? 'Zero': count;
+        const {value} = this.state;
+        return value === 0 ? 'Zero': value;
     }
 }
 
