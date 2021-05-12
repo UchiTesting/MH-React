bb354e8be1bb2f14ae0401b22d83de1004912375
UchiTesing <uchitesting57@gmail.com>
2021-05-12 16:21:31 +0200

    Changed from static call to components to call from a loop.

--- counter-app/src/components/counterSet.jsx
+++ counter-app/src/components/counterSet.jsx
@@ -2,13 +2,26 @@ import React, { Component } from 'react';
 import Counter from './counter';
 
 class CounterSet extends Component {
+
+    state = {
+        counters: [
+            {id:1, value:9},
+            {id:2, value:2},
+            {id:3, value:7},
+            {id:4, value:5},
+            {id:5, value:0}
+        ]
+    }
+
     render() {
         return (
             <div>
-                <Counter />
-                <Counter />
-                <Counter />
-                <Counter />
+                {this.state.counters.map(counter => 
+                <Counter
+                    key={counter.id} 
+                    value={counter.value} 
+                    id={counter.id}
+                />)}
             </div>
         )
     }
