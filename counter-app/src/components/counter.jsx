694fa071c64fd487ddbd641c5ef23b265668d40a
UchiTesing <uchitesting57@gmail.com>
2021-05-17 11:29:19 +0200

    Updated the props to reflect passing a counter instead of multiple separate props.

--- counter-app/src/components/counter.jsx
+++ counter-app/src/components/counter.jsx
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 
 class Counter extends Component {
     state  = {
-        value: this.props.value,
+        value: this.props.counter.value,
         tags: []
     }
 
@@ -14,10 +14,11 @@ class Counter extends Component {
     render() {
         return (
             <React.Fragment>
-                <h6>Counter #{this.props.id}</h6>
+                <h6>Counter #{this.props.counter.id}</h6>
                 <span className={this.getBadgeClasses()}>{this.formatCount()}</span><br/>
                 <button onClick={this.handleDecrement} className="btn btn-secondary btn-sm m-2">Decrement</button>
                 <button onClick={this.handleIncrement} className="btn btn-secondary btn-sm m-2">Increment</button>
+                <button onClick={() => {this.props.onDelete(this.props.counter.id)}} className="btn btn-danger btn-sm m-2">Delete</button>
                 <br/>
             </React.Fragment>
         );
