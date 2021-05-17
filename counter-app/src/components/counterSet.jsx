bbbdaa1a207c696ea2062e9ac56a646c1279a87a
UchiTesing <uchitesting57@gmail.com>
2021-05-17 14:18:45 +0200

    Showcase of the effect of the lack of Single source of truth on Counter component.

--- counter-app/src/components/counterSet.jsx
+++ counter-app/src/components/counterSet.jsx
@@ -16,6 +16,7 @@ class CounterSet extends Component {
     render() {
         return (
             <div>
+                <button onClick={this.handleReset} className="btn btn-primary btn-sm m-2">Reset</button>
                 {this.state.counters.map(counter => 
                 <Counter
                     key={counter.id} 
@@ -30,6 +31,15 @@ class CounterSet extends Component {
 
     handleDelete = (counterId) => {
         const counters = this.state.counters.filter(c => c.id !== counterId);
+        this.setState({counters});
+    }
+
+    handleReset = () => {
+        const counters = this.state.counters.map( c => {
+            c.value = 0;
+            return c;
+        });
+
         this.setState({counters});
     }
 }
