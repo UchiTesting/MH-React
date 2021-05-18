749172f5839ccabb2b9b5c3730ac960ea788452c
UchiTesing <uchitesting57@gmail.com>
2021-05-18 12:38:28 +0200

    Updated NavBar to display the number of counters which value differs from 0.

--- counter-app/src/components/navbar.jsx
+++ counter-app/src/components/navbar.jsx
@@ -5,6 +5,7 @@ class NavBar extends Component {
         return (
             <nav className="navbar navbar-light bg-light">
                 <a className="navbar-brand" href="#">Navbar</a>
+                <span className="badge badge-pill badge-secondary">{this.props.totalCounters}</span>
             </nav>
         )
     }
