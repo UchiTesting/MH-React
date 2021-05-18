bfcc52f8b2b4c54f24a970b852594706bf9e45b4
UchiTesing <uchitesting57@gmail.com>
2021-05-18 15:15:35 +0200

    Used object destructuration to clean references to props.

--- counter-app/src/components/counterSet.jsx
+++ counter-app/src/components/counterSet.jsx
@@ -4,18 +4,18 @@ import Counter from './counter';
 class CounterSet extends Component {
 
     render() {
+        const { onReset, onDelete, onIncrement, onDecrement } = this.props;
+
         return (
             <div>
-                <button onClick={this.props.onReset} className="btn btn-primary btn-sm m-2">Reset</button>
+                <button onClick={onReset} className="btn btn-primary btn-sm m-2">Reset</button>
                 {this.props.counters.map(counter =>
                     <Counter
                         key={counter.id}
-                        // value={counter.value} 
-                        // id={counter.id}
                         counter={counter}
-                        onDelete={this.props.onDelete}
-                        onIncrement={() => { this.props.onIncrement(counter) }}
-                        onDecrement={() => { this.props.onDecrement(counter) }}
+                        onDelete={onDelete}
+                        onIncrement={() => { onIncrement(counter) }}
+                        onDecrement={() => { onDecrement(counter) }}
                     />)}
             </div>
         )
